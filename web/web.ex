ed2891afb576707f6266f715e0a20f243e4d7a9a
Yeong Sheng, Tan <yeongsheng.tan@ruckuswireless.com>
2015-12-27 21:07:01 +0800

    Upgrade to Phoenix 1.1.1

--- web/web.ex
+++ web/web.ex
@@ -18,8 +18,9 @@ defmodule Rumbl.Web do
 
   def model do
     quote do
-      use Ecto.Model
+      use Ecto.Schema
 
+      import Ecto
       import Ecto.Changeset
       import Ecto.Query, only: [from: 1, from: 2]
     end
@@ -30,7 +31,7 @@ defmodule Rumbl.Web do
       use Phoenix.Controller
 
       alias Rumbl.Repo
-      import Ecto.Model
+      import Ecto
       import Ecto.Query, only: [from: 1, from: 2]
 
       import Rumbl.Router.Helpers
@@ -48,6 +49,8 @@ defmodule Rumbl.Web do
       use Phoenix.HTML
 
       import Rumbl.Router.Helpers
+      import Rumbl.ErrorHelpers
+      import Rumbl.Gettext
     end
   end
 
@@ -62,7 +65,7 @@ defmodule Rumbl.Web do
       use Phoenix.Channel
 
       alias Rumbl.Repo
-      import Ecto.Model
+      import Ecto
       import Ecto.Query, only: [from: 1, from: 2]
     end
   end
