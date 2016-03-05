d321c2646b8203a2ec85d6f88c062a7f528c0d07
Yeong Sheng, Tan <yeongsheng.tan@ruckuswireless.com>
2016-03-05 23:14:53 +0800

    feat: session login logout

--- web/router.ex
+++ web/router.ex
@@ -17,9 +17,9 @@ defmodule Rumbl.Router do
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
 
-    get "/",            PageController, :index
-    resources "/users", UserController, only: [:index, :show, :new, :create]
-    # get "/users/:id", UserController, :show
+    get "/",               PageController,    :index
+    resources "/users",    UserController,    only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
